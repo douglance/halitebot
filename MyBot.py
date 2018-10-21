b78ee745280a1cd3ac22065099e31857a505e64f
douglance <doug.lance@gmail.com>
2018-10-21 17:43:14 -0400

    Updated drops to stay 5 distance away

--- MyBot.py
+++ MyBot.py
@@ -34,6 +34,8 @@ while True:
     me = game.me
     game_map = game.game_map
     command_queue = []
+    dropoffs = me.get_dropoffs()
+    dropoffs.append(me.shipyard)
 
     bank = me.halite_amount
     # logging.info('bank: ' + str(me.halite_amount))
@@ -48,10 +50,17 @@ while True:
             game_map[position].value = game_map[position].halite_amount
             all_cells.append(game_map[position])
 
-    def make_drop():
+    def make_drop(bank):
         logging.info('making dropoff')
-        # bank =- constants.DROPOFF_COST
+        bank -= constants.DROPOFF_COST
         return ship.make_dropoff()
+
+    def get_closest_drop():
+        distances = []
+        for dropoff in dropoffs:
+            distances.append((
+                dropoff, game_map.calculate_distance(ship.position, dropoff.position)))
+        return min(distances, key=lambda x: x[1])[0]
 
     def get_random():
         nearby_positions = [
@@ -104,16 +113,8 @@ while True:
     def go_drop():
         try:
             logging.info('going to drop')
-            dropoffs = me.get_dropoffs()
-            dropoffs.append(me.shipyard)
             if len(dropoffs) > 1:
-                logging.info('dropoffs:' + str(dropoffs))
-                distances = []
-                for dropoff in dropoffs:
-                    distances.append((
-                        dropoff, game_map.calculate_distance(ship.position, dropoff.position)))
-                closest_drop = min(distances, key=lambda x: x[1])[0]
-                target = closest_drop.position
+                target = get_closest_drop().position
             else:
                 target = dropoffs[0].position
         except Exception as err:
@@ -124,12 +125,13 @@ while True:
 
     def go_get_halite():
         logging.info('going to get halite')
-        best_cell = find_best()
+        best_cell = get_best_cell()
         try:
             move = game_map.naive_navigate(ship, best_cell.position)
             logging.info(move)
             target_position = ship_position.position.directional_offset(
                 move)
+            logging.info(game_map[target_position].has_structure)
             if not game_map[target_position].has_structure:
                 target = best_cell.position
                 all_cells.remove(best_cell)
@@ -143,7 +145,7 @@ while True:
             return ship.move(
                 game_map.naive_navigate(ship, target))
 
-    def find_best():
+    def get_best_cell():
         best_fitness = 0
         for cell in all_cells:
             distance = (game_map.calculate_distance(
@@ -158,18 +160,16 @@ while True:
         return best_cell
 
     def get_dropability():
-        nearby_positions = [
-            ship_position.position.directional_offset(Direction.North), ship_position.position.directional_offset(Direction.South), ship_position.position.directional_offset(Direction.East), ship_position.position.directional_offset(Direction.West)]
-        nearby_ships = 0
-        for position in nearby_positions:
-            if game_map[position].is_occupied:
-                nearby_ships += 1
-        if nearby_ships is 4:
+        # TODO: Find the best location for drop zone and build it there instead.
+        nearby_ships = get_number_of_nearby_ships()
+        distance = game_map.calculate_distance(
+            ship.position, get_closest_drop().position)
+        if nearby_ships is 4 and distance > 5:
             return True
         else:
             return False
 
-    def get_nearby_ships():
+    def get_number_of_nearby_ships():
         nearby_positions = [
             ship_position.position.directional_offset(Direction.North),
             ship_position.position.directional_offset(Direction.South),
@@ -186,15 +186,15 @@ while True:
             ship_position = game_map[ship.position]
             cost_to_move = .1 * ship_position.halite_amount
 
-            nearby_ships = get_nearby_ships()
+            nearby_ships = get_number_of_nearby_ships()
 
-            if game.turn_number >= constants.MAX_TURNS/1.05 and not ship_position.has_structure:
+            if game.turn_number >= constants.MAX_TURNS/1.075 and not ship_position.has_structure:
                 command = go_drop_final()
-            elif nearby_ships is 4 and bank > constants.DROPOFF_COST and not ship_position.has_structure:
-                command = make_drop()
+            elif get_dropability() and bank > constants.DROPOFF_COST and not ship_position.has_structure:
+                command = make_drop(bank)
             elif nearby_ships < 3 and ship.halite_amount >= 750 - cost_to_move and not ship_position.has_structure:
                 command = go_drop()
-            elif cost_to_move <= ship.halite_amount and not ship_position.has_structure:
+            elif nearby_ships < 3 and cost_to_move <= ship.halite_amount and not ship_position.has_structure:
                 command = go_get_halite()
             elif cost_to_move <= ship.halite_amount:
                 command = go_random()
@@ -208,6 +208,7 @@ while True:
         # logging.info(data)
     if game.turn_number <= constants.MAX_TURNS/1.5 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
+        bank -= constants.SHIP_COST
 
     # if me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
     #     command_queue.append(me.shipyard.spawn())
